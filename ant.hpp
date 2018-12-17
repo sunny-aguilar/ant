d00590152edcb60d831d7d4efbc036f47da3be37
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-16 20:30:49 -0800

    add ant class

---
+++ ant.hpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Jan 2019
+** Description:     Write a short description
+**                  of your code
+**                  here.
+**
+*********************************************************************/
+#ifndef ANT_HPP
+#define ANT_HPP
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -6,13 +6,26 @@
 **                  here.
 **
 *********************************************************************/
+#include "ant.hpp"
 #include <iostream>
+
 using std::cin;
 using std::cout;
 using std::endl;
 
+class Ant {
+private:
+    
+public:
+
+};
+
 int main() {
 
-    
+    // ant rules
+    // If the ant is on a white space, turn right 90 degrees and change the space to black.
+    // If the ant is on a black space, turn left 90 degrees and change the space to white.
+
+
     return 0;
 }
