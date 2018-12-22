456b4a52981b2e714716928bbbae0ae84b9a2fea
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-22 15:40:19 -0800

    play again

--- main.cpp
+++ main.cpp
@@ -44,44 +44,45 @@ int main() {
     //    cout << "Selection made was " << antMenu.getSelection() << endl; // debugging line (may delete)
 
     if (antMenu.getSelection() == "1") {
-        // User picked start simulation option
-        // submenu items - ask user to enter rows and columns for board and validate
-        antMenu.subMenuRows();
-        antBoard.setRows( antMenu.validateNumber(2, 100) );
-        antMenu.subMenuCols();
-        antBoard.setCols( antMenu.validateNumber(2, 100) );
-
-        // initialize board dimensions in a 2-D dynamic array
-        antBoard.setBoardArrayDimensions();
-
-        // submenu item - ask user to enter steps and validate
-        antMenu.subMenuSteps();
-        antBoard.setSteps( antMenu.validateNumber(1, 1000) );
-
-        // submenu item - ask user to select the starting location method
-        antMenu.subMenuStartLocation();
-        if ( antMenu.validateSelection() == "1") {
-            // set custom user-picked location
-            antBoard.setStartRow( antMenu.subMenuRowStartLocation( antBoard.getRows() ) );
-            antBoard.setStartCol( antMenu.subMenuColStartLocation( antBoard.getCols() ) );
-        }
-        else {
-            // set random starting location for the ant
-            // Extra Credit 5%
-            unsigned seed;
-            seed = static_cast<unsigned int>(time(nullptr));
-            srand(seed);
-            antBoard.setStartRow( antMenu.setRandomLocation( antBoard.getRows() ) );
-            antBoard.setStartCol( antMenu.setRandomLocation( antBoard.getCols() ) );
-        }
-
-        // submenu item - ask user to set ant's starting orientation
-        antMenu.subMenuSetAntOrientation();
-        ant.setAntOrientation( antMenu.validateOrientation() );
-
         // once game specs have been entered, the user should have the option
         // to replay the game or end it
         do {
+            // User picked start simulation option
+            // submenu items - ask user to enter rows and columns for board and validate
+            antMenu.subMenuRows();
+            antBoard.setRows( antMenu.validateNumber(2, 100) );
+            antMenu.subMenuCols();
+            antBoard.setCols( antMenu.validateNumber(2, 100) );
+
+            // initialize board dimensions in a 2-D dynamic array
+            antBoard.setBoardArrayDimensions();
+
+            // submenu item - ask user to enter steps and validate
+            antMenu.subMenuSteps();
+            antBoard.setSteps( antMenu.validateNumber(1, 1000) );
+
+            // submenu item - ask user to select the starting location method
+            antMenu.subMenuStartLocation();
+            if ( antMenu.validateSelection() == "1") {
+                // set custom user-picked location
+                antBoard.setStartRow( antMenu.subMenuRowStartLocation( antBoard.getRows() ) );
+                antBoard.setStartCol( antMenu.subMenuColStartLocation( antBoard.getCols() ) );
+            }
+            else {
+                // set random starting location for the ant
+                // Extra Credit 5%
+                unsigned seed;
+                seed = static_cast<unsigned int>(time(nullptr));
+                srand(seed);
+                antBoard.setStartRow( antMenu.setRandomLocation( antBoard.getRows() ) );
+                antBoard.setStartCol( antMenu.setRandomLocation( antBoard.getCols() ) );
+            }
+
+            // submenu item - ask user to set ant's starting orientation
+            antMenu.subMenuSetAntOrientation();
+            ant.setAntOrientation( antMenu.validateOrientation() );
+
+
 
             // show message initializing ant
             antMenu.submenuInitializeAnt();
