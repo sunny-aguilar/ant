d174b7ec1965fba5814b7302ce66e3d061c5496b
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-12-24 13:24:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 ** Date:            Jan 2019
 ** Description:     Write a short description
 **                  of your code
-**                  here. 
+**                  here.
 **
 *********************************************************************/
 #include "ant.hpp"
@@ -21,7 +21,6 @@ using std::string;
 //using std::regex;
 
 int main() {
-
     // ant rules
     // If the ant is on a white space, turn right 90 degrees and change the space to black.
     // If the ant is on a black space, turn left 90 degrees and change the space to white.
@@ -31,63 +30,58 @@ int main() {
 
     // create menu, board, and ant
     Menu antMenu;
-    Board antBoard;     // !!! Move board to inside of do while loop to prevent memory leaks!
-    Ant ant;
 
     // call main menu
     antMenu.showStartMenu();
+
     // get user main menu selection
     antMenu.setSelection( antMenu.validateSelection() );
 
-
+    // *********************************************************************************
+    // main menu screen - user plays game or quits *************************************
+    // *********************************************************************************
     if (antMenu.getSelection() == "1") {
         // do while loop repeats the game if user wants to play again
         do {
-            // ADD BOARD INSTANCE HERE
-            
-            
-            // User picked start simulation option
-            // submenu items - ask user to enter rows and columns for board and validate
-            antMenu.subMenuRows();
+            // create board and ant
+            Board antBoard;                 // create board inside loop otherwise leak!
+            Ant ant{};
+
+            // submenu - ask user to enter rows and columns for board and validate
+            antMenu.submenuRows();
             antBoard.setRows( antMenu.validateNumber(2, 100) );
-            antMenu.subMenuCols();
+            antMenu.submenuCols();
             antBoard.setCols( antMenu.validateNumber(2, 100) );
 
-            // initialize board dimensions in a 2-D dynamic array
-            antBoard.setBoardArrayDimensions();
-
-            // submenu item - ask user to enter steps and validate
-            antMenu.subMenuSteps();
+            // submenu - ask user to enter steps and validate
+            antMenu.submenuSteps();
             antBoard.setSteps( antMenu.validateNumber(1, 1000) );
 
-            // submenu item - ask user to select the starting location method
-            antMenu.subMenuStartLocation();
+            // submenu - ask user to select the starting location for the ant
+            antMenu.submenuStartLocation();
             if ( antMenu.validateSelection() == "1") {
-                // set custom user-picked starting location for the ant
-                antBoard.setStartRow( antMenu.subMenuRowStartLocation( antBoard.getRows() ) );
-                antBoard.setStartCol( antMenu.subMenuColStartLocation( antBoard.getCols() ) );
+                // set custom starting location for the ant
+                antBoard.setStartRow( antMenu.submenuRowStartLocation( antBoard.getRows() ) );
+                antBoard.setStartCol( antMenu.submenuColStartLocation( antBoard.getCols() ) );
             }
             else {
-                // set random starting location for the ant
                 // Extra Credit 5%
+                // set random starting location for the ant
                 unsigned seed;
                 seed = static_cast<unsigned int>(time(nullptr));
                 srand(seed);
-                // set random starting location for the ant
                 antBoard.setStartRow( antMenu.setRandomLocation( antBoard.getRows() ) );
                 antBoard.setStartCol( antMenu.setRandomLocation( antBoard.getCols() ) );
             }
 
-            // submenu item - ask user to set ant's starting orientation
-            antMenu.subMenuSetAntOrientation();
+            // submenu - ask user to set ant's starting orientation
+            antMenu.submenuSetAntOrientation();
             ant.setAntOrientation( antMenu.validateOrientation() );
 
-
-
             // show message initializing ant
-            antMenu.submenuInitializeAnt();
+            antMenu.submenuInitializeAntMessage();
 
-            // debugging code - test board specs
+            // debugging code - test board specs DELETE WHEN DONE
             cout << "\n\nRows " << antBoard.getRows() << " Cols " << antBoard.getCols() << endl;
             cout << "Number of Steps is " << antBoard.getSteps() << endl;
             cout << "Random row start location " << antBoard.getStartRow() << endl;
@@ -100,21 +94,26 @@ int main() {
             // ant class holds current row and column for the ant only
             ant.setCurrentRowLocation( antBoard.getStartRow() );
             ant.setCurrentColLocation( antBoard.getStartCol() );
+            // send current ant location from ant class back to board class
+            antBoard.setNewAntcoor( ant.getCurrentRowLocation(), ant.getCurrentColLocation() );
 
-            // debugging - display current ant row and location
+
+            // debugging - display current ant row and location DELETE WHEN DONE
             cout << "Ant row location " << ant.getCurrentRowLocation() << endl;
             cout << "Ant col location " << ant.getCurrentColLocation() << endl;
 
-            // set board dimensions
+            // set board dimensions rows and columns
+            // allocates 2-D array
             antBoard.setBoardArrayDimensions();
 
-            // set initial board characters
+            // set initial board characters to white ' '
             antBoard.setAllBoardCharacters();
 
             // set ant * location on board (in 2d array)
             // remember current color and set variable in board class
             // setAntLocation function has -1 adjustment included to row and col
-            antBoard.setAntLocation( ant.getCurrentRowLocation(), ant.getCurrentColLocation() );
+            // wait until you can find a way to show the * ant and remember the space color too
+//            antBoard.setAntLocation( ant.getCurrentRowLocation(), ant.getCurrentColLocation() );
 
             // show initial board setup
             cout << "\nInitial board set up\n";
@@ -125,16 +124,38 @@ int main() {
             // Loop through the steps
             for (int steps = 0; steps < antBoard.getSteps(); steps++) {
 
-                // set ant heading
-                ant.setAntOrientation( antBoard.setBoardSpace( ant.getCurrentRowLocation(),
+                // update ant orientation
+                ant.setAntOrientation( antBoard.updateOrientation( ant.getCurrentRowLocation(),
                                                                ant.getCurrentColLocation(),
                                                                ant.getAntOrientation() ) );
+
+
+                // move ant
+                antBoard.moveAnt( ant.getAntOrientation() );    // move and and update new coord in board
+                ant.setCurrentRowLocation( antBoard.getAntCurrentRow() );                  // update new row coord in ant class
+                ant.setCurrentColLocation( antBoard.getAntCurrentCol() );                  // update new col coord in ant class
+//                antBoard.flipColor();                           // flip ant color
+
+
+
+
                 // move ant and set new location in ant class
-                ant.setCurrentRowLocation( antBoard.getAntCurrentRow() );
-                ant.setCurrentColLocation( antBoard.getAntCurrentCol() );
+                // if heading is North, South, East, West
+//                cout << "Current color " << antBoard.getColor( ant.getCurrentRowLocation(),
+//                                                               ant.getCurrentColLocation() ) << endl;
+
+
+//                ant.moveAnt( antBoard.getColor( ant.getCurrentRowLocation(),
+//                                                ant.getCurrentColLocation() ) );
+
+
+
+
+//                ant.setCurrentRowLocation( antBoard.getAntCurrentRow() );
+//                ant.setCurrentColLocation( antBoard.getAntCurrentCol() );
 
                 // delete old * after moving
-                antBoard.deletePriorLocation();
+//                antBoard.deletePriorLocation();
 
                 // add * to board again
                 // setAntLocation function has -1 adjustment to row and col
