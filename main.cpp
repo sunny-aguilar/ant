ac5f91e3c95aee3bb69bc0475122b77baa08d35e
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-29 21:09:44 -0800

    v3

--- main.cpp
+++ main.cpp
@@ -120,7 +120,9 @@ int main() {
             for (int steps = 0; steps < antBoard.getSteps(); steps++) {
                 trackSteps++;
 
-                // update ant orientation for ant going out of bounds
+                // update ant orientation for ant going out of bounds. If ant goes out of bounds,
+                // behavior is to make ant turn around 180 degree and move 1 square in that new
+                // direction 
                 if (antBoard.checkWallHitVar()) {
                     ant.setAntOrientation( antBoard.wallCheckOrientation() );
                     antBoard.setCheckWallHitVar(false);
